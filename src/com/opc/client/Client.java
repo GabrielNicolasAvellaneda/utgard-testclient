ec4897a3b00916c1dfa05d4c301f3ef01c9ceb41
gavellaneda <avellaneda.gabriel@gmail.com>
2015-02-02 17:31:40 -0200

    Initial commit

---
+++ src/com/opc/client/Client.java
@@ -0,0 +1,54 @@
+package com.opc.client;
+
+import java.util.concurrent.Executors;
+
+import org.jinterop.dcom.common.JIException;
+import org.openscada.opc.lib.common.ConnectionInformation;
+import org.openscada.opc.lib.da.AccessBase;
+import org.openscada.opc.lib.da.DataCallback;
+import org.openscada.opc.lib.da.Item;
+import org.openscada.opc.lib.da.ItemState;
+import org.openscada.opc.lib.da.Server;
+import org.openscada.opc.lib.da.SyncAccess;
+
+import jcifs.smb.SmbException;
+
+public class Client {
+
+	 public static void main(final String[] args) throws Exception {
+	        // create connection information
+	        final ConnectionInformation ci = new ConnectionInformation();
+	        ci.setHost("127.0.0.1");
+	        //ci.setDomain("");
+	        //ci.setUser("opc");
+	        //ci.setPassword("opc");
+	        //ci.setProgId("SWToolbox.TOPServer.V5");
+	        ci.setClsid("680DFBF7-C92D-484D-84BE-06DC3DECCD68"); // if ProgId is not working, try it using the Clsid instead
+	        final String itemId = "_System._Time_Second";
+	        // create a new server
+	        final Server server = new Server(ci, Executors.newSingleThreadScheduledExecutor());
+	         
+	        try {
+	            // connect to server
+	            server.connect();
+	            // add sync access, poll every 500 ms
+	            final AccessBase access = new SyncAccess(server, 500);
+	            access.addItem(itemId, new DataCallback() {
+	                @Override
+	                public void changed(Item item, ItemState state) {
+	                    System.out.println(state);
+	                }
+					
+	            });
+	            // start reading
+	            access.bind();
+	            // wait a little bit
+	            Thread.sleep(10 * 1000);
+	            // stop reading
+	            access.unbind();
+	        } catch (final JIException e) {
+	            System.out.println(String.format("%08X: %s", e.getErrorCode(), server.getErrorMessage(e.getErrorCode())));
+	        }
+	    }
+
+}
